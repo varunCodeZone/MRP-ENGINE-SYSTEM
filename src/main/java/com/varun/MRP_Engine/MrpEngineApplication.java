cc043568a43a3df003720ac3b4b3814c12be9d9b
varunCodeZone <kumarvarun26112004@gmail.com>
2026-07-13 20:12:36 +0530

    initial structure of springboot

---
+++ src/main/java/com/varun/MRP_Engine/MrpEngineApplication.java
@@ -0,0 +1,13 @@
+package com.varun.MRP_Engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varun/MRP_Engine/MrpEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.MRP_Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrpEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
